acb57b3bf688ef65e7b12ef028ce290fbcd75a03
oguzalp <oguz.alp@osf.digital>
2020-05-02 16:03:57 +0300

    Fixed messages bundle

--- src/commands/dxload/start.ts
+++ src/commands/dxload/start.ts
@@ -15,7 +15,7 @@ Messages.importMessagesDirectory(__dirname);
 
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
-const messages = Messages.loadMessages('dxload', 'org');
+const messages = Messages.loadMessages('dxloader', 'org');
 
 export default class start extends SfdxCommand {
 
